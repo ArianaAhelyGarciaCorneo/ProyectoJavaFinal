b9809798c94db8548a6fb7ed0d3aada95e0db32d
Ariana <ahelygarcia19@gmail.com>
2025-02-24 20:42:04 -0500

    prueba

---
+++ eiminar/src/main/java/com/idat/eiminar/Eiminar.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.idat.eiminar;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Eiminar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("por el mmento sigue siendo de prueba ");
+    }
+}
