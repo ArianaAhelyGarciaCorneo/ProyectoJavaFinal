811ecbdad565c2c6cfba66fce75e15fcaff20abc
Ariana <ahelygarcia19@gmail.com>
2025-02-24 19:38:43 -0500

    ensallo

---
+++ example/src/main/java/com/idat/example/Example.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.idat.example;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Example {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
