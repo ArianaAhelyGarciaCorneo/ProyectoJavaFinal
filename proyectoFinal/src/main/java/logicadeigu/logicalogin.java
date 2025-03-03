91c4f04eda1729e6c0bc07c765b5de2b4c7f3f79
Ariana <ahelygarcia19@gmail.com>
2025-03-03 18:47:43 -0500

    avance

---
+++ proyectoFinal/src/main/java/logicadeigu/logicalogin.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logicadeigu;
+
+import igu.Login;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class logicalogin {
+    public static void main(String[] args) {
+    Login logi = new Login ();
+    logi.setVisible(true);
+    logi.setLocationRelativeTo(null);
+    
+    
+    }
+    
+    
+}
